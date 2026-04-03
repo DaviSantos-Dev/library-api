643a8eb364d912f0596dea012a1f51c465d696a3
Davi Santos <davicoutinho984@gmail.com>
2026-04-03 08:59:28 -0300

    project start

---
+++ src/test/java/dev/davisantos/libray_api/LibrayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.davisantos.libray_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
