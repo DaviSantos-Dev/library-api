0a5ad25e7d3600caa49516748d2347a9509ff714
Davi Santos <davicoutinho984@gmail.com>
2026-04-03 14:08:29 -0300

    fix: remove from application book creations

--- src/main/java/dev/davisantos/library_api/Application.java
+++ src/main/java/dev/davisantos/library_api/Application.java
@@ -2,6 +2,8 @@ package dev.davisantos.library_api;
 
 import dev.davisantos.library_api.entities.Book;
 import dev.davisantos.library_api.entities.Loan;
+import dev.davisantos.library_api.repository.BookRepository;
+import dev.davisantos.library_api.service.BookService;
 import dev.davisantos.library_api.service.LoanService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,16 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-
-        //Adicionando livros para testar os empréstimos
-        Book book0 = new Book("O Gatilho", "Teófilo Hayashi");
-        Book book1 = new Book("A Cultura do Jejum", "Luciano Subirá");
-        Book book2 = new Book("Você escolherá o deserto", "Samuel whitefield");
-        Book book3 = new Book("Vimos e ouvimos", "Israel Subirá");
-        Book book4 = new Book("O Deus que destrói sonhos", "Rodrigo Bibo");
-
-
 		SpringApplication.run(Application.class, args);
-	}
+
+    }
 
 }
